7fe1880d5b569800e17f270218e9f6394668f836
nurvefa1-wq <nurvefa1@gmail.com>
2026-09-15 22:26:45 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/myvpn/MainActivity.kt
+++ app/src/main/java/com/example/myvpn/MainActivity.kt
@@ -1,21 +1,33 @@
-package com.example.myvpn
+      package com.example.myvpn
 
 import android.app.Activity
 import android.content.Intent
 import android.net.VpnService
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : Activity() {
+
+    private lateinit var statusText: TextView
+    private lateinit var connectButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = VpnService.prepare(this)
+        setContentView(R.layout.activity_main)
 
-        if (intent != null) {
-            startActivityForResult(intent, 0)
-        } else {
-            startVpn()
+        statusText = findViewById(R.id.statusText)
+        connectButton = findViewById(R.id.connectButton)
+
+        connectButton.setOnClickListener {
+            val intent = VpnService.prepare(this)
+
+            if (intent != null) {
+                startActivityForResult(intent, 0)
+            } else {
+                startVpn()
+            }
         }
     }
 
@@ -23,5 +35,8 @@ class MainActivity : Activity() {
         startService(
             Intent(this, MyVpnService::class.java)
         )
+
+        statusText.text = "VPN açık"
+        connectButton.text = "VPN ÇALIŞIYOR"
     }
 }
