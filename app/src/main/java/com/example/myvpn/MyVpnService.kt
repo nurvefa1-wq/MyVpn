f3b04cbcb9edfe3b7bf4f8e052e58338bf147979
nurvefa1-wq <nurvefa1@gmail.com>
2026-09-15 22:17:24 +0500

    Create app/src/main/java/com/example/myvpn/MyVpnService.kt

---
+++ app/src/main/java/com/example/myvpn/MyVpnService.kt
@@ -0,0 +1,30 @@
+package com.example.myvpn
+
+import android.content.Intent
+import android.net.VpnService
+import android.os.ParcelFileDescriptor
+
+class MyVpnService : VpnService() {
+
+    private var vpnInterface: ParcelFileDescriptor? = null
+
+    override fun onStartCommand(
+        intent: Intent?,
+        flags: Int,
+        startId: Int
+    ): Int {
+
+        vpnInterface = Builder()
+            .setSession("MyVPN")
+            .addAddress("10.0.0.2", 32)
+            .establish()
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        vpnInterface?.close()
+        vpnInterface = null
+        super.onDestroy()
+    }
+}
