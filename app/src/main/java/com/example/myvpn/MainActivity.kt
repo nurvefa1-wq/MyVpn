26db20affd26412971a2c8f9599d01163a64cd95
nurvefa1-wq <nurvefa1@gmail.com>
2026-09-16 08:02:37 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/myvpn/MainActivity.kt
+++ app/src/main/java/com/example/myvpn/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.myvpn
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        val text = TextView(this)
+        text.text = "MyVPN çalışıyor!"
+        text.textSize = 28f
+        text.setPadding(40, 40, 40, 40)
+
+        setContentView(text)
     }
 }
